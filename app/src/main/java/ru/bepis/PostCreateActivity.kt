18001979a9467f9a4f85e06edf8323b7df059329
pihanya <gostevvm@yandex.ru>
2020-09-18 19:07:14 +0300

    Initialize project

---
+++ app/src/main/java/ru/bepis/PostCreateActivity.kt
@@ -0,0 +1,11 @@
+package ru.bepis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PostCreateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_create)
+    }
+}
