47e651c0cddc2c5019f0c959afdba04f7a4e0987
Igor Anokhin <igr.anokhin@gmail.com>
2020-09-19 06:11:35 +0300

    add simple navigation

--- app/src/main/java/ru/bepis/FeedActivity.kt
+++ app/src/main/java/ru/bepis/FeedActivity.kt
@@ -1,11 +1,18 @@
 package ru.bepis
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class FeedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feed)
+    }
+
+    fun onMapButtonClicked(view: View) {
+        val intent = Intent(this, MapActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/ru/bepis/PostCreateActivity.kt
+++ app/src/main/java/ru/bepis/PostCreateActivity.kt
@@ -1,11 +1,18 @@
 package ru.bepis
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class PostCreateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post_create)
+    }
+
+    fun onPostCreateClicked(view: View) {
+        val intent = Intent(this, FeedActivity::class.java)
+        startActivity(intent)
     }
 }
