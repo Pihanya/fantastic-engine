4d1e2f99e3fe8a9d57bcb4ddbeafe357d7bc6940
pihanya <gostevvm@yandex.ru>
2020-09-19 15:25:34 +0300

    Add choosing of mood on the first screen

--- app/src/main/java/ru/bepis/PostCreateActivity.kt
+++ app/src/main/java/ru/bepis/PostCreateActivity.kt
@@ -1,14 +1,24 @@
 package ru.bepis
 
+import android.app.AlertDialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_post_create.*
+
 
 class PostCreateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post_create)
+
+        moodLayout.setOnClickListener {
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+            builder.setTitle("Выберите настроение")
+            builder.setItems(R.array.emotions) { _, _ -> }
+            builder.show()
+        }
     }
 
     fun onPostCreateClicked(view: View) {
